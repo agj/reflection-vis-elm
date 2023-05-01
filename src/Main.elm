b605357a46c28c82807092a3acc9c4cf812f155c
agj <ale@agj.cl>
2023-04-30 23:04:14 -0400

    Display mirror and box

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,20 @@
 module Main exposing (main)
 
+import Angle
 import Browser
 import Color
+import Direction2d
 import Geometry.Svg as Svg
 import Html exposing (Html)
 import Html.Attributes
 import LineSegment2d exposing (LineSegment2d)
+import Pixels exposing (Pixels)
 import Point2d
+import Rectangle2d exposing (Rectangle2d)
 import TypedSvg as Svg
 import TypedSvg.Attributes as SvgAttr
-import TypedSvg.Types exposing (Paint(..), px)
+import TypedSvg.Types exposing (Paint(..), Transform(..), pc, px)
+import Vector2d
 
 
 main =
@@ -52,15 +57,24 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    let
+        mirror : LineSegment2d Pixels c
+        mirror =
+            LineSegment2d.from
+                (Point2d.pixels 0 -100)
+                (Point2d.pixels 0 100)
+
+        box : Rectangle2d Pixels c
+        box =
+            Rectangle2d.withDimensions
+                ( Pixels.float 50, Pixels.float 50 )
+                (Angle.degrees 20)
+                (Point2d.pixels -100 0)
+    in
     Html.div [ Html.Attributes.class "container" ]
-        [ Svg.svg
-            []
-            [ LineSegment2d.from (Point2d.pixels 0 0)
-                (Point2d.pixels 100 100)
-                |> Svg.lineSegment2d
-                    [ SvgAttr.stroke (Paint Color.lightPurple)
-                    , SvgAttr.strokeWidth (px 3)
-                    ]
+        [ Svg.svg [ SvgAttr.viewBox -500 -500 1000 1000 ]
+            [ mirror |> viewMirror
+            , box |> viewBox
             ]
         , Html.node "style"
             []
@@ -76,3 +90,20 @@ view model =
                 """
             ]
         ]
+
+
+viewMirror : LineSegment2d u c -> Html Msg
+viewMirror segment =
+    segment
+        |> Svg.lineSegment2d
+            [ SvgAttr.stroke (Paint Color.lightPurple)
+            , SvgAttr.strokeWidth (px 3)
+            ]
+
+
+viewBox : Rectangle2d u c -> Html Msg
+viewBox rectangle =
+    rectangle
+        |> Svg.rectangle2d
+            [ SvgAttr.fill (Paint Color.lightPurple)
+            ]
