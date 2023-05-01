bea54b2160a4a7ebc41b8045b699fd5786bc0b3e
agj <ale@agj.cl>
2023-04-30 20:58:24 -0400

    Paint something on screen

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,14 @@
 module Main exposing (main)
 
 import Browser
+import Color
+import Geometry.Svg as Svg
 import Html exposing (Html)
+import LineSegment2d exposing (LineSegment2d)
+import Point2d
+import TypedSvg as Svg
+import TypedSvg.Attributes as SvgAttr
+import TypedSvg.Types exposing (Paint(..), px)
 
 
 main =
@@ -44,4 +51,13 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    Html.text "Nothing yet"
+    Svg.svg
+        [ SvgAttr.width (px 500)
+        , SvgAttr.height (px 500)
+        ]
+        [ LineSegment2d.from (Point2d.pixels 0 0)
+            (Point2d.pixels 100 100)
+            |> Svg.lineSegment2d
+                [ SvgAttr.stroke (Paint Color.lightPurple)
+                ]
+        ]
